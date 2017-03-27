63909fd33412c643ba2447de3670fb993cba6bad
Valechha <VALECD3@TDBFG.COM>
2017-03-26 20:10:17 -0400

    updated code for the controller

--- src/main/java/com/org/config/controller/ConfigController.java
+++ src/main/java/com/org/config/controller/ConfigController.java
@@ -3,14 +3,36 @@ package com.org.config.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.ws.Response;
 
 @RestController
 @RequestMapping(value = "/api")
 public class ConfigController {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/")
-    public ResponseEntity<String> testEndPoint() {
+    public static final String BASE_PATH = "/api";
+
+    public static final String APPCODE_CONFIG = "{appcode}/config";
+
+    public static final String VERSION = "{version}";
+
+    @RequestMapping(method = RequestMethod.GET, value = BASE_PATH + APPCODE_CONFIG + VERSION)
+    @ResponseBody
+    public ResponseEntity<?> getAppConfig() {
+        return ResponseEntity.ok().body("Success");
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = BASE_PATH + APPCODE_CONFIG)
+    @ResponseBody
+    public ResponseEntity<?> getAppConfigs() {
+        return ResponseEntity.ok().body("Success");
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = BASE_PATH + APPCODE_CONFIG + VERSION)
+    @ResponseBody
+    public ResponseEntity<?> createOrUpdateAppConfig() {
         return ResponseEntity.ok().body("Success");
     }
 }
